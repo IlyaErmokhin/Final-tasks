8b45a9f6fa221a97f95e67b5d089a9a1d0327eda
IlyaErmokhin <63489397+IlyaErmokhin@users.noreply.github.com>
2023-02-06 17:12:16 +0400

    Add files via upload

---
+++ MutliTable/Program.cs
@@ -0,0 +1,24 @@
+﻿public class MultiTable
+{
+    static void Main(string[] args)
+    {
+        Console.Write("      |");
+        for (int i = 1; i < 11; i++)
+        {
+            Console.Write("{0, 6}", i + "|");
+        }
+        Console.WriteLine();
+        Console.WriteLine("-------------------------------------------------------------------");
+
+        for (int i = 1; i < 11; ++i)
+        {
+            Console.Write("{0, 6}|", i);
+            for (int j = 1; j < 11; ++j)
+            {
+                Console.Write("{0, 6}", i * j + "|");
+            }
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------------------------------------");
+        }
+    }
+}
