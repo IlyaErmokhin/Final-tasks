5584ec578bb8a3e2875b8088f3225b5627e49322
IlyaErmokhin <63489397+IlyaErmokhin@users.noreply.github.com>
2023-02-06 17:04:46 +0400

    Add files via upload

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrimeNumber;
+
+
+public class isPrimeNumber
+{
+    static void Main(string[] args)
+    {
+        bool PrimeNumber = true;
+        Console.WriteLine("Введите число");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Число {n} {(isPrimeNumber.PrimeNumb(n) ? "простое" : "не является простым")}");
+    }
+    public static bool PrimeNumb(int n)
+    {
+        int k = 0;
+        k = n / 2;
+        for (int i = 2; i <= k; i++)
+        {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
