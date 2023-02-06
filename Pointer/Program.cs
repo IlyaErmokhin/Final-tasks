dc0377c6876fa869ee074d965e1b2b51eee6b00b
IlyaErmokhin <63489397+IlyaErmokhin@users.noreply.github.com>
2023-02-06 17:07:50 +0400

    Add files via upload

---
+++ Pointer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Arrow
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            var keyInfo = Console.ReadKey();
+            switch (keyInfo.Key)
+                
+            {
+                case ConsoleKey.UpArrow:
+                    Console.Clear();       
+                    Console.WriteLine("↑");
+                    break;
+                case ConsoleKey.DownArrow:
+                    Console.Clear();
+                    Console.WriteLine("↓");
+                    break;
+                case ConsoleKey.RightArrow:
+                    Console.Clear();
+                    Console.WriteLine("->");
+                    break;
+                case ConsoleKey.LeftArrow:
+                    Console.Clear();
+                    Console.WriteLine("<-");
+                    break;
+            }
+        }
+    }
+}
