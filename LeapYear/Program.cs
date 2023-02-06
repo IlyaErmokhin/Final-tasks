446303a7f372e3be841cc05d50f0b6d5a2541b60
IlyaErmokhin <63489397+IlyaErmokhin@users.noreply.github.com>
2023-02-06 17:05:54 +0400

    Add files via upload

---
+++ LeapYear/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class LipYear
+{
+    public static void Main(string[] args)
+    {
+        int k;
+        Console.Write("Enter the year: ");
+        k = int.Parse(Console.ReadLine());
+        Console.Write($" {k} {(lipYear(k) ? "год - високосный" : "год - невисокосный")}");
+
+    }
+
+    public static bool lipYear(int k)
+    {
+        if (k % 4 == 0 && (k % 100 != 0 || k % 400 == 0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
