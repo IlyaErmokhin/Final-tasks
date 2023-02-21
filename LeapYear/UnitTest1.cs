b3075ba9d11db5b4cdd3c3bedafc0a0d33bec348
IlyaErmokhin <63489397+IlyaErmokhin@users.noreply.github.com>
2023-02-21 22:07:38 +0400

    Add files via upload

---
+++ LeapYear/UnitTest1.cs
@@ -0,0 +1,24 @@
+namespace LeapYear.Tests;
+
+public class Tests
+{
+    [Test]
+    [TestCase(2000)] // multiple of 400
+    [TestCase(1004)] // multiple of 4 but not 100
+    public void YearIsLeapReturnsTrue(int value)
+    {
+        var result = LeapYear.IsLeapYear(value);
+        
+        Assert.That(DateTime.IsLeapYear(value), Is.EqualTo(result));
+    }
+
+    [Test]
+    [TestCase(2001)] // not multiple of 4
+    [TestCase(1900)] // multiple of 100 but not 400
+    public void YearIsNotLeapReturnsFalse(int value)
+    {
+        var result = LeapYear.IsLeapYear(value);
+        
+        Assert.That(DateTime.IsLeapYear(value), Is.EqualTo(result));
+    }
+}
